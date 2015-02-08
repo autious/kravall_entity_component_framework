23861ac00ca81b8cf9918c2a21eb5f7fb5e7f9f0
Max Danielsson <max@autious.net>
2015-02-08 20:12:45 +0100

    Removes lingering components

--- Components/DrawableComponent.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct DrawableComponent
-{
-    const char* texturePath;
-
-    static const char* GetName() { return "DrawableComponent"; }
-};
--- Components/PlayOnCollisionComponent.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct PlayOnCollisionComponent
-{
-    bool active = true;
-
-    static const char* GetName() { return "PlayOnCollisionComponent"; }
-};
